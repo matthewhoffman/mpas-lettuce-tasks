74c8d1365a32d50092d4425f93a02d50eb01923e
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2013-07-22 15:17:39 -0600

    Initial setup of Lettuce files.
    Includes features:
    	bit-reproducibility
    	bit-restartability

---
+++ bit-restartability.feature
@@ -0,0 +1,23 @@
+Feature: Bit-Restartable simulations
+	MPAS-Ocean simulations are bit-repstartable
+	across different decompositions.
+
+	Scenario: 2 vs 4 procs with split explicit
+		Given A 10000m 20levs "baroclinic_channel" "split_explicit" test
+		Given A 2 processor MPAS run
+		Given A 4 processor MPAS run with restart
+		When I compute the RMS of "temperature"
+		When I compute the RMS of "layerThickness"
+		Then I see "temperature" RMS of 0
+		Then I see "layerThickness" RMS of 0
+		Then I clean the test directory
+
+	Scenario: 1 vs 8 procs with RK4
+		Given A 10000m 20levs "baroclinic_channel" "RK4" test
+		Given A 1 processor MPAS run
+		Given A 8 processor MPAS run with restart
+		When I compute the RMS of "temperature"
+		When I compute the RMS of "layerThickness"
+		Then I see "temperature" RMS of 0
+		Then I see "layerThickness" RMS of 0
+		Then I clean the test directory
---
+++ mpas_tasks.py
@@ -0,0 +1,263 @@
+import sys, os, glob, shutil, numpy, math
+import subprocess
+
+from netCDF4 import *
+from netCDF4 import Dataset as NetCDFFile
+from pylab import *
+
+from lettuce import *
+
+from collections import defaultdict
+
+dev_null = open(os.devnull, 'w')
+
+def seconds_to_timestamp(seconds):
+	days = 0
+	hours = 0
+	minutes = 0
+	
+	if seconds > 24*3600:
+		days = int(seconds/(24*3600))
+		seconds = seconds - int(days * 24 * 3600)
+
+	if seconds > 3600:
+		hours = int(seconds/3600)
+		seconds = seconds - int(hours*3600)
+
+	if seconds > 60:
+		minutes = int(seconds/60)
+		seconds = seconds - int(minutes*60)
+
+	timestamp = "%4.4d_%2.2d:%2.2d:%2.2d"%(days, hours, minutes, seconds)
+	return timestamp
+
+@step('A (\d+)m (\d+)levs "([^"]*)" "([^"]*)" test')
+def get_test_case(step, size, levs, test, time_stepper):
+	world.basedir = os.getcwd()
+	world.rundir = "baroclinic_channel_%sm_20levs"%size
+	world.num_runs = 0
+
+	if not os.path.exists("%s/%s.tgz"%(world.basedir, world.rundir)):
+		command = "wget"
+		arg1 = "http://oceans11.lanl.gov/mpas_data/mpas_ocean/test_cases/release_1.0/baroclinic_channel_%sm_20levs.tgz"%(size)
+		subprocess.call([command, arg1], stdout=dev_null, stderr=dev_null)
+
+	if not os.path.exists("%s/%s"%(world.basedir, world.rundir)):
+		command = "tar"
+		arg1 = "xzf"
+		arg2 = "baroclinic_channel_%sm_20levs.tgz"%size
+		subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+		command = "cp"
+		arg1 = "%s/namelist.input"%world.rundir
+		arg2 = "%s/namelist.input.default"%world.rundir 
+		subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	os.chdir(world.rundir)
+	command = "ln"
+	arg1 = "-s"
+	arg2 = "/var/lib/jenkins/jobs/MPAS-Ocean/workspace/ocean_model"
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	command = "cp"
+	arg1 = "namelist.input.default"
+	arg2 = "namelist.input"
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	command = "rm"
+	arg1 = "-f"
+	arg2 = '\*.output.nc'
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	namelistfile = open('namelist.input', 'r+')
+	lines = namelistfile.readlines()
+
+	for line in lines:
+		if line.find("config_dt") >= 0:
+			line_split = line.split(" = ")
+			world.dt = float(line_split[1])
+		if line.find("config_time_integrator") >= 0:
+			line_split = line.split(" = ")
+			world.old_time_stepper = line_split[1].replace("'","")
+
+	world.time_stepper_change = False
+	if world.old_time_stepper.find(time_stepper) < 0:
+		world.time_stepper_change = True
+		if world.old_time_stepper.find("split_explicit") >= 0:
+			world.dt /= 10.0
+		elif time_stepper.find("split_explicit") >= 0:
+			world.dt *= 10.0
+
+	duration = seconds_to_timestamp(int(world.dt*2))
+
+	namelistfile.seek(0)
+	namelistfile.truncate()
+
+	for line in lines:
+		new_line = line
+		if line.find("config_run_duration") >= 0:
+			new_line = "    config_run_duration = '%s'\n"%(duration)
+		elif line.find("config_output_interval") >= 0:
+			new_line = "    config_output_interval = '0000_00:00:01'\n"
+		elif line.find("config_restart_interval") >= 0:
+			new_line = "    config_restart_interval = '1000_00:00:01'\n"
+		elif line.find("config_stats_interval") >= 0:
+			new_line = "    config_stats_interval = '1000_00:00:01'\n"
+		elif line.find("config_dt") >= 0:
+			new_line = "    config_dt = %f\n"%world.dt
+		elif world.time_stepper_change:
+			if line.find("config_time_integrator") >= 0:
+				new_line = "    config_time_integrator = '%s'\n"%(time_stepper)
+
+		namelistfile.write(new_line)
+
+	namelistfile.close()
+
+	del lines
+
+	os.chdir(world.basedir)
+
+
+@step('A (\d+) processor MPAS run')
+def run_mpas(step, procs):
+	os.chdir(world.basedir)
+	os.chdir(world.rundir)
+	command = "mpirun"
+	arg1 = "-n"
+	arg2 = "%s"%procs
+	arg3 = "ocean_model"
+	subprocess.call([command, arg1, arg2, arg3], stdout=dev_null, stderr=dev_null)
+	command = "mv"
+	arg1 = "output.0000-01-01_00.00.00.nc"
+	arg2 = "%sprocs.output.nc"%procs
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+	if world.num_runs == 0:
+		world.num_runs = 1
+		world.run1 = arg2
+		try:
+			del world.rms_values
+			world.rms_values = defaultdict(list)
+		except:
+			world.rms_values = defaultdict(list)
+	elif world.num_runs == 1:
+		world.num_runs = 2
+		world.run2 = arg2
+	os.chdir(world.basedir)
+
+@step('A (\d+) processor MPAS run with restart')
+def run_mpas_with_restart(step, procs):
+	os.chdir(world.basedir)
+	os.chdir(world.rundir)
+
+	duration = seconds_to_timestamp(world.dt)
+	final_time = seconds_to_timestamp(world.dt + 24*3600)
+
+	namelistfile = open('namelist.input', 'r+')
+	lines = namelistfile.readlines()
+	namelistfile.seek(0)
+	namelistfile.truncate()
+
+	for line in lines:
+		if line.find('config_start_time') >= 0:
+			new_line = "    config_start_time = 'file'\n"
+		elif line.find('config_run_duration') >= 0:
+			new_line = "    config_run_duration = '%s'\n"%duration
+		elif line.find('config_restart_interval') >= 0:
+			new_line = "    config_restart_interval = '0000_00:00:01'\n"
+		elif line.find('config_output_interval') >= 0:
+			new_line = "    config_output_interval = '0000_00:00:01'\n"
+		else:
+			new_line = line
+
+		namelistfile.write(new_line)
+
+	namelistfile.close()
+	del lines
+
+	restart_file = open('restart_timestamp', 'w+')
+	restart_file.write('0000-01-01_00:00:00')
+	restart_file.close()
+
+	command = "mpirun"
+	arg1 = "-n"
+	arg2 = "%s"%procs
+	arg3 = "ocean_model"
+	subprocess.call([command, arg1, arg2, arg3], stdout=dev_null, stderr=dev_null)
+
+	command = "rm"
+	arg1 = "-f"
+	arg2 = "output.0000-01-01_00.00.00.nc"
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	namelistfile = open('namelist.input', 'r+')
+	lines = namelistfile.readlines()
+	namelistfile.seek(0)
+	namelistfile.truncate()
+
+	for line in lines:
+		if line.find('config_do_restart') >= 0:
+			new_line = "    config_do_restart = .true.\n"
+		elif line.find('config_restart_interval') >= 0:
+			new_line = "    config_restart_interval = '1000_00:00:01'\n"
+		else:
+			new_line = line
+
+		namelistfile.write(new_line)
+
+	namelistfile.write("mv output.0000-01-%s.nc %sprocs.restarted.output.nc"%(final_time[2:].replace(":","."), procs))
+	namelistfile.close()
+	del lines
+
+	command = "mpirun"
+	arg1 = "-n"
+	arg2 = "%s"%procs
+	arg3 = "ocean_model"
+	subprocess.call([command, arg1, arg2, arg3], stdout=dev_null, stderr=dev_null)
+
+	command = "mv"
+	arg1 = "output.0000-01-%s.nc"%(final_time[2:].replace(":","."))
+	arg2 = "%sprocs.restarted.output.nc"%procs
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
+
+	if world.num_runs == 0:
+		world.num_runs = 1
+		world.run1 = arg2
+		try:
+			del world.rms_values
+			world.rms_values = defaultdict(list)
+		except:
+			world.rms_values = defaultdict(list)
+	elif world.num_runs == 1:
+		world.num_runs = 2
+		world.run2 = arg2
+	os.chdir(world.basedir)
+
+
+@step('I compute the RMS of "([^"]*)"')
+def compute_rms(step, variable):
+	f1 = NetCDFFile("%s/%s"%(world.rundir,world.run1),'r')
+	f2 = NetCDFFile("%s/%s"%(world.rundir,world.run2),'r')
+
+	field1 = f1.variables["%s"%variable][-1,:,:]
+	field2 = f2.variables["%s"%variable][-1,:,:]
+
+	field1 = field1 - field2
+	field1 = field1 * field1
+	rms = sum(field1)
+	rms = rms / sum(field1.shape[:])
+	rms = math.sqrt(rms)
+	world.rms_values[variable].append(rms)
+	f1.close()
+	f2.close()
+	os.chdir(world.basedir)
+
+@step('I see "([^"]*)" RMS of 0')
+def check_rms_values(step, variable):
+	assert world.rms_values[variable][0] == 0.0, '%s RMS failed.'%variable
+
+
+@step('I clean the test directory')
+def clean_test(step):
+	command = "rm"
+	arg1 = "-rf"
+	arg2 = world.rundir
+	subprocess.call([command, arg1, arg2], stdout=dev_null, stderr=dev_null)
